b36581dcfb5135cfe739e2aed8b7e99f3cba7a03
AndrieTan <60122551+AndrieTan@users.noreply.github.com>
2020-10-11 18:26:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,26 +14,26 @@
                 <a href="#" class = "logo"> Dark Space</a>
               </div>
             <div class = "nav">
-                <a href="#" class = "button">HOME</a>
-                <a href="#" class = "button">ABOUT US</a>
-                <a href="#" class = "button">CONTACT US</a>
+                <a href="#sun" class = "button">SUN</a>
+                <a href="#mars" class = "button">MARS</a>
+                <a href="#pluto" class = "button">PLUTO</a>
             </div>
             <div class="content">
-                <div class="floating_text"><center>THE SUN IS NOT WHAT YOU THINK</center></div>
+                 <a id= "sun"><div class="floating_text"><center>THE SUN IS NOT WHAT YOU THINK</center></div></a>
                 <div class="sun"></div>
                 <div class="text">
                     <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie viverra dui at malesuada. Donec id nunc sed arcu fringilla ornare non vitae mi. Praesent venenatis orci nec dui aliquet convallis. Sed posuere lorem eu rutrum convallis. Aenean faucibus mauris eget orci porta suscipit. Praesent non laoreet sapien, vel euismod tellus. Maecenas gravida quam accumsan ante congue, ut fermentum arcu pharetra. Aliquam erat volutpat. Nam odio erat, hendrerit vitae ante eu, gravida rhoncus sapien. Etiam luctus pharetra felis, id gravida lectus luctus vestibulum. Vivamus blandit varius velit quis ultricies. Etiam interdum vehicula metus, tincidunt pretium nunc pulvinar tincidunt. Nulla eu ipsum in ante cursus dictum. In hac habitasse platea dictumst. Etiam et ultricies orci, sed pretium orci.</h1>
                 </div>
             </div>
             <div class="content">
-                <div class="floating_text">MARS IS THE NEW EARTH</div>
+                <a id= "mars"><div class="floating_text">MARS IS THE NEW EARTH</div></a>
                 <div class="mars"></div>
                 <div class="text">
                     <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie viverra dui at malesuada. Donec id nunc sed arcu fringilla ornare non vitae mi. Praesent venenatis orci nec dui aliquet convallis. Sed posuere lorem eu rutrum convallis. Aenean faucibus mauris eget orci porta suscipit. Praesent non laoreet sapien, vel euismod tellus. Maecenas gravida quam accumsan ante congue, ut fermentum arcu pharetra. Aliquam erat volutpat. Nam odio erat, hendrerit vitae ante eu, gravida rhoncus sapien. Etiam luctus pharetra felis, id gravida lectus luctus vestibulum. Vivamus blandit varius velit quis ultricies. Etiam interdum vehicula metus, tincidunt pretium nunc pulvinar tincidunt. Nulla eu ipsum in ante cursus dictum. In hac habitasse platea dictumst. Etiam et ultricies orci, sed pretium orci.</h1>
                 </div>
             </div>
             <div class="content">
-                <div class="floating_text">LONELY PLUTO</div>
+                <a id= "pluto"><div class="floating_text">LONELY PLUTO</div></a>
                 <div class="pluto"></div>
                 <div class="text">
                     <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie viverra dui at malesuada. Donec id nunc sed arcu fringilla ornare non vitae mi. Praesent venenatis orci nec dui aliquet convallis. Sed posuere lorem eu rutrum convallis. Aenean faucibus mauris eget orci porta suscipit. Praesent non laoreet sapien, vel euismod tellus. Maecenas gravida quam accumsan ante congue, ut fermentum arcu pharetra. Aliquam erat volutpat. Nam odio erat, hendrerit vitae ante eu, gravida rhoncus sapien. Etiam luctus pharetra felis, id gravida lectus luctus vestibulum. Vivamus blandit varius velit quis ultricies. Etiam interdum vehicula metus, tincidunt pretium nunc pulvinar tincidunt. Nulla eu ipsum in ante cursus dictum. In hac habitasse platea dictumst. Etiam et ultricies orci, sed pretium orci.</h1>
